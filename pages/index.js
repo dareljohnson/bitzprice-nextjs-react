516d5307d999fde091414de3e276b9bcb91cfb7a
Darel Johnson <dareljohnson@msn.com>
2020-05-15 23:56:42 -0400

    alpha version

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,39 @@
+import fetch from 'isomorphic-fetch'
+import promise from 'es6-promise'
 import Layout from '../components/Layout'
+import Prices from '../components/Prices'
 
-const Index = () => (
+
+const Index = (props) => (
     <Layout>
         <div>
             <h1>
                 Welcome to Bitzprice
             </h1>
+            <p>Check current Bitcoin rate</p>
+            <Prices bpi={props.data.bpi} />
         </div>
-    </Layout>
-    
+    </Layout>  
 )
+
+Index.getInitialProps = async function () {
+    promise.polyfill()
+    const res = await fetch('https://api.coindesk.com/v1/bpi/currentprice.json', 
+    {
+        method: "GET",
+        headers: {
+           
+        }
+    });
+    const data = await res.json();
+
+    return {
+        data: data
+    };
+}
+
+
+
+
 
 export default Index
