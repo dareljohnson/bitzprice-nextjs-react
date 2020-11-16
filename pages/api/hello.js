edae68ba3f85a380b6d6e6c8ded9e2678ac7cac3
Darel Johnson <dareljohnson@msn.com>
2020-11-16 10:45:38 -0500

    Updated PORT

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,9 +1,11 @@
+// serverless function
 
 export default (request, response) => {
   const { name = 'World' } = request.query
   response.status(200).send(`Hello ${name}!`)
-}
+
 
 // Example deployment:
 // https://bitzprice-nextjs-react.now.sh/api/hello?name=reader
 
+}
